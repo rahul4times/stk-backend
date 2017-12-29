437d46ff8b9acb04a2a78f54091f9ee49133bab3
rahul4times <tkdmaster.sharma@gmail.com>
2017-12-29 16:32:30 -0700

    gainer added

--- app.js
+++ app.js
@@ -11,22 +11,43 @@ const io = socketIo(server);
 
 io.on("connection", socket => {
   console.log("New client connected"), setInterval(
-    () => getApiAndEmit(socket),
-    100
+    () => oneCompanyOnly(socket),
+    500
   );
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
-const getApiAndEmit = async socket => {
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => topGainer(socket),
+    500
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+
+
+
+const oneCompanyOnly = async socket => {
   try {
     const res = await axios.get(
       "https://api.iextrading.com/1.0/stock/ssc/quote"
     );
-
-    socket.emit("FromAPI", res.data.latestPrice);
+    socket.emit("FromAPI", res.data);
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
 };
 
+const topGainer = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.iextrading.com/1.0/stock/market/list/gainers"
+    );
+    
+    socket.emit("topGainerList", res.data);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
 server.listen(port, () => console.log(`Listening on port ${port}`));
