00b2219c7036b7e0d4f63fe17e69d3f222ebc929
rahul4times <tkdmaster.sharma@gmail.com>
2017-12-29 12:04:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4000;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    100
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.iextrading.com/1.0/stock/ssc/quote"
+    );
+
+    socket.emit("FromAPI", res.data.latestPrice);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
