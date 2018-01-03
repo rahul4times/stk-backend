25f0190ec56cdadd6d5be09f01fdf74fd5a8f382
rahul4times <tkdmaster.sharma@gmail.com>
2018-01-02 18:31:59 -0700

    multiple api calls added

--- app.js
+++ app.js
@@ -9,45 +9,38 @@ app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server);
 
+
+
 io.on("connection", socket => {
+
   console.log("New client connected"), setInterval(
     () => oneCompanyOnly(socket),
     500
   );
+
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
-
-io.on("connection", socket => {
-  console.log("New client connected"), setInterval(
-    () => topGainer(socket),
-    500
-  );
-  socket.on("disconnect", () => console.log("Client disconnected"));
-});
-
-
 
 
 const oneCompanyOnly = async socket => {
   try {
+
     const res = await axios.get(
+      "https://api.iextrading.com/1.0/stock/aapl/quote"
+    );
+
+    socket.emit("FromAPI", res.data);
+
+    const res2 = await axios.get(
       "https://api.iextrading.com/1.0/stock/ssc/quote"
     );
-    socket.emit("FromAPI", res.data);
+
+    socket.emit("SSCApi", res2.data);
+
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
 };
 
-const topGainer = async socket => {
-  try {
-    const res = await axios.get(
-      "https://api.iextrading.com/1.0/stock/market/list/gainers"
-    );
-    
-    socket.emit("topGainerList", res.data);
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
+
 server.listen(port, () => console.log(`Listening on port ${port}`));
